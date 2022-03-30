9943f4a1682a262c61827848a9d40a8efebabc3b
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2022-03-30 13:04:07 +0530

    Catagory CRUD

--- KnowledgeHubPortal.MVCWebApplication/Controllers/CatagoriesController.cs
+++ KnowledgeHubPortal.MVCWebApplication/Controllers/CatagoriesController.cs
@@ -25,8 +25,34 @@ namespace KnowledgeHubPortal.MVCWebApplication.Controllers
             List<Catagory> catagories =  db.Catagories.ToList();
             // pass catagories to view and display
             // create view
+            //ViewBag.Catagories = catagories;
+            return View(catagories);
+            // List(catagories);
+        }
 
+        public IActionResult Create()
+        {
+            // send a view to collect catagory details from user
             return View();
+        }
+        public IActionResult Save(Catagory catagory)
+        {
+            // receive the data from view
+            // validate the data
+            // Rules: name is must, desc should not have more than 500 chr
+
+            //if (string.IsNullOrEmpty(catagory.CatagoryName) || catagory.Description.Length > 500)
+            if(!ModelState.IsValid)
+                return View("Create");
+
+            // send the data to model for saving in db
+            db.Catagories.Add(catagory);
+            db.SaveChanges();
+            // send a view to list all catagories
+            //List<Catagory> catagories = db.Catagories.ToList();
+
+            //return View("List",catagory);
+            return RedirectToAction("List");
         }
     }
 }
--- KnowledgeHubPortal.MVCWebApplication/Models/Catagory.cs
+++ KnowledgeHubPortal.MVCWebApplication/Models/Catagory.cs
@@ -1,11 +1,18 @@
-﻿namespace KnowledgeHubPortal.MVCWebApplication.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KnowledgeHubPortal.MVCWebApplication.Models
 {
     // Entity Class - Domain Class
     public class Catagory
     {
         public int CatagoryID { get; set; }
+        [Required (ErrorMessage ="Kindly enter the catagory name")]
         public string CatagoryName { get; set; }
+        [MaxLength(500)]
         public string Description { get; set; }
+        //[RegularExpression("")]
+        //[Range(10,10000)]
+        //[EmailAddress]
 
     }
 }
