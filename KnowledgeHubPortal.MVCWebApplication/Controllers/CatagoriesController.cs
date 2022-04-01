19b91cd325e92603fe57728393d7d8bca07d3b6c
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2022-04-01 14:50:00 +0530

    async deno

--- KnowledgeHubPortal.MVCWebApplication/Controllers/CatagoriesController.cs
+++ KnowledgeHubPortal.MVCWebApplication/Controllers/CatagoriesController.cs
@@ -1,11 +1,15 @@
 ﻿using KnowledgeHubPortal.MVCWebApplication.Data;
 using KnowledgeHubPortal.MVCWebApplication.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace KnowledgeHubPortal.MVCWebApplication.Controllers
 {
+    [Authorize(Roles ="admin")]
     public class CatagoriesController : Controller
     {
 
@@ -19,16 +23,17 @@ namespace KnowledgeHubPortal.MVCWebApplication.Controllers
 
 
         // /Catagories/List
-        public IActionResult List(string searchTerm)
+        [AllowAnonymous]
+        public async Task<IActionResult> List(string searchTerm)
         {
             // get all catagories from db
             List<Catagory> catagories = null;
             if (string.IsNullOrWhiteSpace(searchTerm))
-                catagories = db.Catagories.ToList();
+                catagories = await db.Catagories.ToListAsync();
             else
-                catagories = (from c in db.Catagories
+                catagories = await (from c in db.Catagories
                               where c.CatagoryName.Contains(searchTerm) || c.Description.Contains(searchTerm)
-                              select c).ToList(); //LINQ
+                              select c).ToListAsync(); //LINQ
 
                                 // LINQPAd
 
@@ -38,13 +43,12 @@ namespace KnowledgeHubPortal.MVCWebApplication.Controllers
             return View(catagories);
             // List(catagories);
         }
-
         public IActionResult Create()
         {
             // send a view to collect catagory details from user
             return View();
         }
-        public IActionResult Save(Catagory catagory)
+        public async Task<IActionResult> Save(Catagory catagory)
         {
             // receive the data from view
             // validate the data
@@ -56,53 +60,48 @@ namespace KnowledgeHubPortal.MVCWebApplication.Controllers
 
             // send the data to model for saving in db
             db.Catagories.Add(catagory);
-            db.SaveChanges();
+            await db.SaveChangesAsync();
             // send a view to list all catagories
             //List<Catagory> catagories = db.Catagories.ToList();
 
             //return View("List",catagory);
             return RedirectToAction("List");
         }
-
-
-        public IActionResult Delete(int id)
+        public async Task<IActionResult> Delete(int id)
         {
             // get the remaining info from db
-            Catagory catagoryToDelete = db.Catagories.Find(id);
+            Catagory catagoryToDelete = await db.Catagories.FindAsync(id);
             return View(catagoryToDelete);
             //db.Catagories.Remove(catagoryToDelete);
             //db.SaveChanges();
 
             //return View();
         }
-
-        public IActionResult DeleteConfirm(int id)
+        public async Task<IActionResult> DeleteConfirm(int id)
         {
             Catagory catagoryToDelete = db.Catagories.Find(id);
             db.Catagories.Remove(catagoryToDelete);
-            db.SaveChanges();
+            await db.SaveChangesAsync();
 
             TempData["Message"] = $"{catagoryToDelete.CatagoryName} is successfully deleted";
             return RedirectToAction("List");
         }
-
-        public IActionResult Edit(int id)
+        public async Task<IActionResult> Edit(int id)
         {
             // get the remaining catagory info from db
-            var catagoryToEdit = db.Catagories.Find(id);
+            var catagoryToEdit = await db.Catagories.FindAsync(id);
             // return a view to edit the data
             return View(catagoryToEdit);
 
         }
-
-        public IActionResult Update(Catagory catagoryToUpdate)
+        public async Task<IActionResult> Update(Catagory catagoryToUpdate)
         {
             if (!ModelState.IsValid)
                 return View("Edit", catagoryToUpdate);
 
             // send the data to model for updating in db
             db.Entry(catagoryToUpdate).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
-            db.SaveChanges();
+            await db.SaveChangesAsync();
             // send a view to list all catagories
             //List<Catagory> catagories = db.Catagories.ToList();
             TempData["Message"] = $"{catagoryToUpdate.CatagoryName} is successfully updated";
